668cb3a28cfeb3a43cf1e2ec8da4bf3695b61f2a
Martin David Corral Castro <zeromarthk@hotmail.com>
2023-10-16 11:30:33 -0600

    c2:Dependencias yaml

--- api_monster_hunter/lib/providers/mh_provider.dart
+++ api_monster_hunter/lib/providers/mh_provider.dart
@@ -1,10 +1,8 @@
-//import 'dart:convert';
 import 'package:api_monster_hunter/providers/mh_fought.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:api_monster_hunter/models/data.dart';
 import 'package:api_monster_hunter/models/now_playing_response.dart';
-//import 'package:api_monster_hunter/models/popular_response.dart';
 
 class MhProvider extends ChangeNotifier {
   List<Ailment> onDisplayMh = [];
@@ -18,7 +16,6 @@ class MhProvider extends ChangeNotifier {
   getOnDisplayMh() async {
     var url = Uri.https('mhw-db.com/monsters');
     final response = await http.get(url);
-
     final nowPLayingResponse = NowPlayingResponse.fromRawJson(response.body);
     onDisplayMh = nowPLayingResponse.ailments;
     notifyListeners();
@@ -26,12 +23,8 @@ class MhProvider extends ChangeNotifier {
 
   getFoughtMh() async {
     var url = Uri.https('mhw-db.com/monsters');
-
     final response = await http.get(url);
-
     final popularResponse = FoughtMh.fromRawJson(response.body);
-
-    //Destructurar resultado para cambiar pagina y mantener los actuales
     foughtMh = [...foughtMh, ...popularResponse.data];
     notifyListeners();
   }
